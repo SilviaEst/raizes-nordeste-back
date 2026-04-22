7f2bdfed1740a73045d6e6c2d262e2d9bdbb4545
Silvia Akiba <akibasilvia@gmail.com>
2026-04-22 17:51:00 -0300

    ajuste no código do arquivo pedido_routes.py

--- src/api/routes/pedido_routes.py
+++ src/api/routes/pedido_routes.py
@@ -4,6 +4,7 @@ from typing import List
 from src.domain.entities.pedido import Pedido, RespostaPedidoCriado
 from src.application.services.pedido_service import PedidoService
 from src.infrastructure.database.database import get_db 
+from fastapi import HTTPException
 
 router = APIRouter()
 service = PedidoService()
@@ -15,4 +16,6 @@ def criar_novo_pedido(pedido: Pedido, db: Session = Depends(get_db)):
 
 @router.get("/", response_model=List[Pedido], tags=["Pedidos"])
 def listar_todos_os_pedidos(db: Session = Depends(get_db)):
+    usuario_perfil = "CLIENTE"
+
     return service.listar_pedidos(db)
